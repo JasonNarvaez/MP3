c4e79d8e9102d841377adca9e27e47d5b3af5f14
Jason Narvaez <thejason@tamu.edu>
2015-10-22 17:39:46 -0500

    Update proctest.cpp

--- proctest.cpp
+++ proctest.cpp
@@ -11,6 +11,7 @@
 #include <sys/stat.h>
 #include <iomanip>
 #include <fstream>
+#include <vector>
 
 #include <errno.h>
 #include <unistd.h>
@@ -52,53 +53,144 @@ int main(int argc, char ** argv) {
 	//system();
 	
 	
+	string temp;
+	//string temp= "cat /proc/"+PID+"/maps";//output maps 
+	//system(temp.c_str());
 	
-	string temp= "cat /proc/"+PID+"/maps";//output maps 
-	system(temp.c_str());
-	
-	
-	temp= "grep -i 'pid' /proc/"+PID+"/status";//output PID
+	cout<<"1)"<<endl;
+	temp= "grep -i pid /proc/"+PID+"/status";//output PID
 	system(temp.c_str());
 	
 	//need to output EUID, EGID, RUID, RGID, FSUID, FSGID
-	cout<<"RUID, EUID, SSUID, FSUID"<<endl;
-	temp= "grep -i 'UID' /proc/"+PID+"/status";
+	cout<<endl<<"RUID, EUID, SSUID, FSUID"<<endl;
+	temp= "grep -i UID /proc/"+PID+"/status";
 	system(temp.c_str());
 	
-	cout<<"RGID, EGID, SSGID, FSGID"<<endl;
-	temp= "grep 'Gid' /proc/"+PID+"/status";
+	cout<<endl<<"RGID, EGID, SSGID, FSGID"<<endl;
+	temp= "grep Gid /proc/"+PID+"/status";
 	system(temp.c_str());
 	
-	temp= "grep -i 'state' /proc/"+PID+"/status";	//output state	
+	cout<<endl<<"2)"<<endl;
+	temp= "grep -i state /proc/"+PID+"/status";	//output state	
 	system(temp.c_str());
 	
-	temp= "grep -i 'thread' /proc/"+PID+"/status";	//output thread info	
+	cout<<endl<<"3)"<<endl;
+	temp= "grep -i thread /proc/"+PID+"/status";	//output thread info	
 	system(temp.c_str());
 	
-	cout<<endl<<"NOTE: start counting at [1], not [0]!"<<endl;
-	cout<<"[14]: utime, [15]: stime, [16]: cutime, [17]: cstime, [18]: Priority number, [19]: Nice value"<<endl;
-	temp= "cat /proc/"+PID+"/stat";//priority and nice value
-	system(temp.c_str());
+	//cout<<endl<<"NOTE: start counting at [1], not [0]!"<<endl;
+	//cout<<"[14]: utime, [15]: stime, [16]: cutime, [17]: cstime, [18]: Priority number, [19]: Nice value"<<endl;
+	//temp= "cat /proc/"+PID+"/stat";//priority and nice value
 	
-	cout<<endl<<"[26]: startcode, [27]: endcode, [29]: ESP, [30]: EIP"<<endl;
-	system(temp.c_str());
+	//cout<<endl<<"[26]: startcode, [27]: endcode, [29]: ESP, [30]: EIP"<<endl;
 	
-	cout<<endl<<"File handle:"<<endl;
-	temp= "grep -i 'FDSize' /proc/"+PID+"/status";	//output file handle	
+	
+	cout<<endl;
+	cout<<"4) & 5) & 6)"<<endl;
+	temp = "/proc/"+PID+"/stat";
+	ifstream ifs(temp.c_str());
+	vector<string> data;//stat file has 44 things
+	string extract;
+	int i = 0;
+	
+	while(ifs.good()){
+		ifs>>extract;
+		
+		if(i == 13 || i == 14 || i == 15 || i == 16 || i == 17 || i == 18  || i == 25 || i == 26 || i == 28 || i == 29){
+			data.push_back(extract);
+		}
+		i++;
+	}
+	for(int i=0;i<data.size();i++){
+		switch(i){
+			case 0:
+				cout<<"utime: "<<data[i]<<endl;
+				break;
+			case 1:
+				cout<<"stime: "<<data[i]<<endl;
+				break;
+			case 2:
+				cout<<"cutime: "<<data[i]<<endl;
+				break;
+			case 3:
+				cout<<"cstime: "<<data[i]<<endl;
+				break;
+			case 4:
+				cout<<"Priority number: "<<data[i]<<endl;
+				break;
+			case 5:
+				cout<<"Nice number: "<<data[i]<<endl;
+				break;
+			case 6:
+				cout<<"startcode: "<<data[i]<<endl;
+				break;
+			case 7:
+				cout<<"endcode: "<<data[i]<<endl;
+				break;
+			case 8:
+				cout<<"ESP: "<<data[i]<<endl;
+				break;
+			case 9:
+				cout<<"EIP: "<<data[i]<<endl;
+				break;
+		}
+	}
+	
+	cout<<endl<<"7)"<<endl;
+	cout<<"File handle:"<<endl;
+	temp= "grep -i FDSize /proc/"+PID+"/status";	//output file handle	
 	system(temp.c_str());
 	
 	cout<<endl<<"Context switches:"<<endl;
-	temp= "grep -i 'switches' /proc/"+PID+"/status";	//output context switches	
+	temp= "grep -i switches /proc/"+PID+"/status";	//output context switches	
 	system(temp.c_str());
 	
-	cout<<endl<<"Processors:"<<endl;
-	temp= "grep -i 'Cpus_allowed' /proc/"+PID+"/status";	//output Processors	
+	cout<<endl<<"8)"<<endl;
+	cout<<"Processors:"<<endl;
+	temp= "grep -i Cpus_allowed /proc/"+PID+"/status";	//output Processors	
 	system(temp.c_str());
-	// ifstream ifs(temp.c_str());
-	// while(good){
+	cout<<endl;
+	temp= "egrep 'heap|stack|vdso|vsyscall' /proc/"+PID+"/maps > mapsinfo.txt";//output maps to a file
+	system(temp.c_str());
+	
+	cout<<"9)"<<endl;
+	temp = "mapsinfo.txt";
+	ifstream ifs2(temp.c_str());
+	vector<string> mapinfo;//stat file has 44 things
+	string mapdata;
+	i = 0;
+	
+	while(ifs2.good()){
+		ifs2>>mapdata;
+		if(!ifs2.good()){break;}
+		switch(i){
+			case 0:
+				cout<<"address range: "<<mapdata<<endl;
+				break;
+			case 1:
+				cout<<"permissions: "<<mapdata<<endl;
+				break;
+			case 2:
+				cout<<"offset: "<<mapdata<<endl;
+				break;
+			case 3:
+				cout<<"dev: "<<mapdata<<endl;
+				break;
+			case 4:
+				cout<<"inode: "<<mapdata<<endl;
+				break;
+			case 5:
+				cout<<"path name: "<<mapdata<<endl<<endl;
+				i = -1;
+				break;
+			
+		}
 		
-		
-	// }
+		i++;
+	}
+	
+	
+	
 	//context switch is in status, last two (voluntary and involuntary)
 	//1 2 3 4 5 7
 }
